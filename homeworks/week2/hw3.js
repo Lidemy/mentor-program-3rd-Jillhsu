23193aadede672f170e3ff5f6e646ae3ce945e62
Jill <jillhsu51@gmail.com>
2019-04-22 23:58:18 +0800

    hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
 function reverse(str) {
-  return str;
+    var ans = ''
+    for (var i = str.length - 1; i >= 0; i--)
+       ans += str[i]
+    console.log(ans)
+  return ans;
 }
 
 reverse('hello');
+reverse('yoyoyo')
+reverse('1abc2')
+reverse('1,2,3,2,1')
