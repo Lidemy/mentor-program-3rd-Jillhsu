b1fa43649deb8f5d2747cb6b423af4b983cb643f
Jill <jillhsu51@gmail.com>
2019-04-22 23:42:19 +0800

    hw2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,16 @@
 function capitalize(str) {
-  return str;
+    var ans = ''
+    if(str[0] >= 'a' && str[0] <= 'z')
+         ans += str[0].toUpperCase()
+    else
+        ans += str[0]
+    for ( var i = 1; i < str.length; i++)
+         ans += str[i]
+
+    return ans;
 }
 
 console.log(capitalize('hello'));
+console.log(capitalize('nick'));
+console.log(capitalize('Nick'));
+console.log(capitalize(',hello'));
