25c60f9d8d6e589a7f90fa95869cbd67daff7785
Jill <jillhsu51@gmail.com>
2019-04-24 00:35:08 +0800

    hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,27 @@
 function join(str, concatStr) {
-  return concatStr;
+    var add = concatStr
+    concatStr = ''
+    for ( i = 0; i < str.length; i++)
+        if ( i != str.length - 1)
+            concatStr += str[i] + add 
+        else
+            concatStr += str[i]
+    return concatStr;
 }
 
 function repeat(str, times) {
+    var num = times
+    times = ''
+    for ( var i = 0; i < num; i++)
+        times += str
   return times;
 }
 
 console.log(join('a', '!'));
 console.log(repeat('a', 5));
+console.log(join([1, 2, 3], ''))
+console.log(join(["a", "b", "c"], "!"))
+console.log(join(["a", 1, "b", 2, "c", 3], ','))
+
+console.log(repeat('a', 5))
+console.log(repeat('yoyo', 2))
