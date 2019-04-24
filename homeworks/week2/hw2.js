3c39a8711460dc9566ba20f6091bd368474807b2
Jill <jillhsu51@gmail.com>
2019-04-24 13:53:23 +0800

    hw2 eslint

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,13 +1,15 @@
 function capitalize(str) {
-    var ans = ''
-    if(str[0] >= 'a' && str[0] <= 'z')
-         ans += str[0].toUpperCase()
-    else
-        ans += str[0]
-    for ( var i = 1; i < str.length; i++)
-         ans += str[i]
-
-    return ans;
+  let i;
+  let ans = '';
+  if (str[0] >= 'a' && str[0] <= 'z') {
+    ans += str[0].toUpperCase();
+  } else {
+    ans += str[0];
+  }
+  for (i = 1; i < str.length; i += 1) {
+    ans += str[i];
+  }
+  return ans;
 }
 
 console.log(capitalize('hello'));
