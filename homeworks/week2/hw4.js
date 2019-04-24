bbdfce2167aa7f3b8e89f741dfc29d01cfded0c1
Jill <jillhsu51@gmail.com>
2019-04-24 13:57:14 +0800

    hw4 eslint

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,7 +1,10 @@
 function printFactor(n) {
-    for (var i = 1; i <= n; i++)
-        if ( n % i === 0)
-            console.log(i)
+  let i;
+  for (i = 1; i <= n; i += 1) {
+    if (n % i === 0) {
+      console.log(i);
+    }
+  }
   return n;
 }
 
