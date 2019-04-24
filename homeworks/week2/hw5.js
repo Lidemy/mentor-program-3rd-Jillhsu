f3f97a0bafdd90903241f2fddbb83d8cdd08cea6
Jill <jillhsu51@gmail.com>
2019-04-24 14:11:23 +0800

    hw5 eslint

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,27 +1,31 @@
 function join(str, concatStr) {
-    var add = concatStr
-    concatStr = ''
-    for ( i = 0; i < str.length; i++)
-        if ( i != str.length - 1)
-            concatStr += str[i] + add 
-        else
-            concatStr += str[i]
-    return concatStr;
+  const add = concatStr;
+  let i;
+  let ans = '';
+  for (i = 0; i < str.length; i += 1) {
+    if (i !== str.length - 1) {
+      ans += str[i] + add;
+    } else {
+      ans += str[i];
+    }
+  }
+  return ans;
 }
 
 function repeat(str, times) {
-    var num = times
-    times = ''
-    for ( var i = 0; i < num; i++)
-        times += str
-  return times;
+  const num = times;
+  let i;
+  let ans = '';
+  for (i = 0; i < num; i += 1) {
+    ans += str;
+  }
+  return ans;
 }
 
 console.log(join('a', '!'));
 console.log(repeat('a', 5));
-console.log(join([1, 2, 3], ''))
-console.log(join(["a", "b", "c"], "!"))
-console.log(join(["a", 1, "b", 2, "c", 3], ','))
-
-console.log(repeat('a', 5))
-console.log(repeat('yoyo', 2))
+console.log(join([1, 2, 3], ''));
+console.log(join(['a', 'b', 'c'], '!'));
+console.log(join(['a', 1, 'b', 2, 'c', 3], ','));
+console.log(repeat('a', 5));
+console.log(repeat('yoyo', 2));
