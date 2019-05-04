e16f1ce7168e6811b007c6f6449492458e5aa854
SweetLark <33699354+Jillhsu@users.noreply.github.com>
2019-05-04 22:55:18 +0800

    Update hw4.js

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,13 @@
-function isPalindromes(str) {
-  console.log(str);
+function isPrime(n) {
+  if (n === 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isPalindromes;
