cedf709795b9afc46c9266c6c53c9f123f5df0a1
SweetLark <33699354+Jillhsu@users.noreply.github.com>
2019-05-04 09:39:54 +0800

    Update hw2.js

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,15 @@
 function alphaSwap(str) {
-  console.log(str);
+  let ans = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      ans += str[i].toUpperCase();
+    } else if (str[i] >= 'A' && str[i] <= 'Z') {
+      ans += str[i].toLowerCase();
+    } else {
+      ans += str[i];
+    }
+  }
+  return ans;
 }
 
 module.exports = alphaSwap;
