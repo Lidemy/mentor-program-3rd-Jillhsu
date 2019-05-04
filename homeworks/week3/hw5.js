381fa1af4cd8adf14ba551e0ac5e179bad016d7e
SweetLark <33699354+Jillhsu@users.noreply.github.com>
2019-05-04 09:52:29 +0800

    Update hw5.js

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,5 +1,25 @@
 function add(a, b) {
-  console.log(a, b);
+  const add0 = Math.abs(str1.length - str2.length);
+  const first = (str1.length >= str2.length) ? str1 : str2;
+  let second = (str1.length >= str2.length) ? str2 : str1;
+  let ans = '';
+  if (add0) {
+    for (let i = add0; i > 0; i -= 1) {
+      second = `0${second}`;
+    }
+  }
+  console.log(first);
+  console.log(second);
+  let carry = 0;
+  for (let i = first.length - 1; i >= 0; i -= 1) {
+    const addint = Number(first[i]) + Number(second[i]) + carry;
+    carry = (addint > 9) ? 1 : 0;
+    ans = String(addint % 10) + ans;
+  }
+  if (carry) {
+    ans = `1${ans}`;
+  }
+  return ans;
 }
 
 module.exports = add;
