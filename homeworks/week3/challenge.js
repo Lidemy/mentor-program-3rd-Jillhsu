7b7495830d1c2070c1216e5643cd62d456a56c97
sweetlark <sweetlark@localhost.localdomain>
2019-05-05 21:37:39 +0800

    challenge_ver1

--- homeworks/week3/challenge.js
+++ homeworks/week3/challenge.js
@@ -38,14 +38,10 @@ function multi(str1, str2) {
       const addint = Number(str1[i]) * Number(str2[j]) + carry;
       carry = (addint - (addint % 10)) / 10;
       next = String(addint % 10) + next;
-      if (j === str2.length - 1 && carry > 0) {
-        next = String(carry) + next;
-      }
+      if (j === str2.length - 1 && carry > 0) next = String(carry) + next;
     }
     if (i === str1.length - 1) base = next;
-    for (let add0 = str1.length - 1 - i; add0 > 0; add0 -= 1) {
-      next = `${next}0`;
-    }
+    for (let add0 = str1.length - 1 - i; add0 > 0; add0 -= 1) next = `${next}0`;
     if (str1.length - 1 - i) base = add(base, next);
   }
   return base;
