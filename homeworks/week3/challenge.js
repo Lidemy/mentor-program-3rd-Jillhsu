7cf2a192e83c4d59ac484225a96db94283da27a6
sweetlark <sweetlark@localhost.localdomain>
2019-05-10 09:23:03 +0800

    test

--- homeworks/week3/challenge.js
+++ homeworks/week3/challenge.js
@@ -28,7 +28,6 @@ function multi(str1, str2) {
       const addint = Number(str1[i]) * Number(str2[j]) + carry;
       carry = (addint - (addint % 10)) / 10;
       next = String(addint % 10) + next;
-      if (j === str2.length - 1 && carry) next = String(carry) + next;
     }
     if (i === str1.length - 1) base = next;
     for (let add0 = str1.length - 1 - i; add0 > 0; add0 -= 1) next = `${next}0`;
@@ -37,10 +36,3 @@ function multi(str1, str2) {
   return base;
 }
 module.exports = multi;
-/*
-   console.log(multi('123', '123'));
-   console.log(multi('999999999999999999999999999', '1'));
-   console.log(multi('1111', '1111111111'));
-   console.log(multi('3', '5'));
-   console.log(multi('111111111111111111111111111111', '222222222222222222222222222222'));
-*/
